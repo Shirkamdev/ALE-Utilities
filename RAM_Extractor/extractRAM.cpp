77e24c2f41a02e22ac46ac7a8d3f009e8af40b1f
ALEJANDRO REYES ALBILLAR <el_reyes_95@hotmail.com>
2017-01-05 18:05:45 +0100

    Added improvement of the code and debugging of the code

--- RAM_Extractor/extractRAM.cpp
+++ RAM_Extractor/extractRAM.cpp
@@ -1,5 +1,5 @@
 /*
-  This program was indented to help ani ALE developer.
+  This program was indented to help any ALE developer.
 
   Copyright (C) 2016 Shirkamdev Xnetrix 
 
@@ -39,6 +39,9 @@
 #include <bitset>
 #include <iostream>
 
+#include <memory>
+#include <stdexcept>
+
 #include "StdPijo.h"
 #include "ale_interface.hpp"
 
@@ -58,10 +61,10 @@
 // KEY DEFINITIONS //
 /////////////////////
 
-#define KEY_UP 17 //W
-#define KEY_DOWN 31 //S
-#define KEY_LEFT 30 //A
-#define KEY_RIGHT 32 //D
+#define KEY_UP 72 //8 del teclado numerico
+#define KEY_DOWN 80 //2 del teclado numerico
+#define KEY_LEFT 75 //4 del teclado numerico
+#define KEY_RIGHT 77 //6 del teclado numerico
 #define KEY_FIRE 57 //SPACE
 
 ////////////////////////////
@@ -72,7 +75,6 @@
 #define RAM_FILE_BINARY "binRAMExtract_"
 #define FILE_TERMINATION ".csv"
 
-#define INPUT_KEYBOARD "/dev/input/event2"
 
 static const char *const evval[3] = {
     "RELEASED",
@@ -80,7 +82,9 @@ static const char *const evval[3] = {
     "REPEATED"
 };
 
-const int MAX_STEPS = 7500;
+#define INPUT_KEYBOARD "/dev/input/" 
+
+const int MAX_STEPS = 99500;
 int previousRam[NUM_BYTES_RAM];
 int previousRAMFrames[NUM_BYTES_RAM];
 ALEInterface alei;
@@ -93,111 +97,131 @@ ofstream file, fileBin;
 std::thread lector;
 
 void ctrlCHandler(int s){
-   lector.detach();
-   STDP::Terminar();
+	lector.detach();
+	STDP::Terminar();
 
-   std::cout << "-- PROGRAM FINISHED --\n";
-   exit(1);
+	std::cout << "-- PROGRAM FINISHED --\n";
+	exit(1);
 }
 
 void usage(char* pname) {
-   std::cerr
-      << "\nUSAGE:\n" 
-      << "   " << pname << " <romfile>\n";
-   exit(-1);
+	std::cerr
+	  << "\nUSAGE:\n" 
+	  << "   " << pname << " <romfile>\n";
+	exit(-1);
 }
 
 void printRam(){
-   ostringstream convert;
-   string result;
+	ostringstream convert;
+	string result;
 
-   clear();
-   STDP::PonCursor(0, 0);
-   STDP::CambiaColor(STDP_A_NEGRITA, STDP_C_VERDE, STDP_C_NEGRO);
-   STDP::EscribeStr("-- RAM LECTOR --");
-   STDP::Refrescar();
-   STDP::PonCursor(0, 1); //Put cursor on 0, 0 
-   STDP::CambiaColor(STDP_A_NORMAL, STDP_C_VERDE, STDP_C_NEGRO);
+	clear();
+	STDP::PonCursor(0, 0);
+	STDP::CambiaColor(STDP_A_NEGRITA, STDP_C_VERDE, STDP_C_NEGRO);
+	STDP::EscribeStr("-- RAM LECTOR --");
+	STDP::Refrescar();
+	STDP::PonCursor(0, 1); //Put cursor on 0, 0 
+	STDP::CambiaColor(STDP_A_NORMAL, STDP_C_VERDE, STDP_C_NEGRO);
 
-   //Write super indexes
-   STDP::EscribeCh(' ');
-   for(int i=0; i<16; ++i) {
-      convert.str("");
-      convert.clear();
-      convert << " "<< setfill('0') << setw(2) << std::hex << i;
-      result = convert.str();
-      STDP::EscribeStr(result.c_str());
-   }
+	//Write super indexes
+	STDP::EscribeCh(' ');
+	for(int i=0; i<16; ++i) {
+	  convert.str("");
+	  convert.clear();
+	  convert << " "<< setfill('0') << setw(2) << std::hex << i;
+	  result = convert.str();
+	  STDP::EscribeStr(result.c_str());
+	}
 
-   int pos = 2;
-   STDP::PonCursor(0, pos);
-   for(int i = 0 ;i < NUM_BYTES_RAM; ++i){
-      if(previousRam[i] != -1) {
-         if(i % 16 == 0) {
-            STDP::PonCursor(0, pos);
-            convert.str("");
-            convert.clear();
-            convert << std::hex << (pos-2) << " ";
-            result = convert.str();
-            STDP::EscribeStr(result.c_str());
-            ++pos;
-         }
+	int pos = 2;
+	STDP::PonCursor(0, pos);
+	for(int i = 0 ;i < NUM_BYTES_RAM; ++i){
+	  if(previousRam[i] != -1) {
+	     if(i % 16 == 0) {
+	        STDP::PonCursor(0, pos);
+	        convert.str("");
+	        convert.clear();
+	        convert << std::hex << (pos-2) << " ";
+	        result = convert.str();
+	        STDP::EscribeStr(result.c_str());
+	        ++pos;
+	     }
 
-         if(!(previousRam[i] == alei.getRAM().get(i) && previousRAMFrames[i] < 1)) { //To maintain format
-            STDP::CambiaColor(STDP_A_NORMAL, STDP_C_AZUL, STDP_C_NEGRO);
-         }
+	     if(!(previousRam[i] == alei.getRAM().get(i) && previousRAMFrames[i] < 1)) { //To maintain format
+	        STDP::CambiaColor(STDP_A_NORMAL, STDP_C_AZUL, STDP_C_NEGRO);
+	     }
 
-         convert.str("");
-         convert.clear();
-         convert << setfill('0') << setw(2) << std::hex << (int)alei.getRAM().get(i) << " ";
-         result = convert.str();
-         STDP::EscribeStr(result.c_str());
+	     convert.str("");
+	     convert.clear();
+	     convert << setfill('0') << setw(2) << std::hex << (int)alei.getRAM().get(i) << " ";
+	     result = convert.str();
+	     STDP::EscribeStr(result.c_str());
 
-         STDP::CambiaColor(STDP_A_NORMAL, STDP_C_VERDE, STDP_C_NEGRO);
-      }
+	     STDP::CambiaColor(STDP_A_NORMAL, STDP_C_VERDE, STDP_C_NEGRO);
+	  }
 
-      previousRam[i] = alei.getRAM().get(i);
-   }
+	  previousRam[i] = alei.getRAM().get(i);
+	}
 
-   STDP::Refrescar();
+	STDP::Refrescar();
 }
 
-void ramToFile() {
-
+void ramToFile(std::string name) {
     //Printing in HEX (0x)
-   for(int i = 0 ;i < NUM_BYTES_RAM; ++i) {
-      int valor = alei.getRAM().get(i);
+	file.open(RAM_FILE+name+FILE_TERMINATION,ios::out | ios::app);
+	for(int i = 0 ;i < NUM_BYTES_RAM; ++i) {
+	  int valor = alei.getRAM().get(i);
+	  file << valor;
+	  //file << std::hex << valor;
+	  if(i != NUM_BYTES_RAM-1) {
+	     file << ";";
+	  }
+	  else {
+	     file << "\n"; //Last value to print (CSV format)
+	  }
+	}
+	file.close();
+	//Printing in binnary
+	/*fileBin.open(RAM_FILE_BINARY+name+FILE_TERMINATION, ios::out | ios::app);
+	for(int i = 0 ;i < NUM_BYTES_RAM; ++i) {
+	  int valor = alei.getRAM().get(i);
+	  fileBin << std::bitset<8>(valor);
+	  if(i != NUM_BYTES_RAM-1) {
+	     fileBin << ";";
+	  }
+	  else {
+	     fileBin << "\n"; //
+	  }
+	}
+	fileBin.close();*/
+}
 
-      file << std::hex << valor;
-      if(i != NUM_BYTES_RAM-1) {
-         file << ",";
-      }
-      else {
-         file << "\n"; //Last value to print (CSV format)
-      }
-   }
-
-   //Printing in binnary
-   for(int i = 0 ;i < NUM_BYTES_RAM; ++i) {
-      int valor = alei.getRAM().get(i);
-
-      fileBin << std::bitset<8>(valor);
-      if(i != NUM_BYTES_RAM-1) {
-         fileBin << ",";
-      }
-      else {
-         fileBin << "\n"; //
-      }
-   }
+std::string exec(const char* cmd) {
+    char buffer[128];
+    std::string result = "";
+    std::shared_ptr<FILE> pipe(popen(cmd, "r"), pclose);
+    if (!pipe) throw std::runtime_error("popen() failed!");
+    while (!feof(pipe.get())) {
+        if (fgets(buffer, 128, pipe.get()) != NULL)
+            result += buffer;
+    }
+    result=result.substr(0,result.find("\n"));
+    return result;
 }
 
 void keyboardRead() {
-   const char *dev = INPUT_KEYBOARD;
+	//Obtain the kbd event route
+
+ 	std::string aout=INPUT_KEYBOARD;
+ 	//aout+="event2";
+ 	aout+=exec("cat /proc/bus/input/devices | awk '/keyboard/{for(a=0;a>=0;a++){getline;{if(/kbd/==1){ print \n$NF;exit 0;}}}}'| cut -d' ' -f4"); 
+
+    const char *dev = aout.c_str();
     struct input_event ev;
     ssize_t n;
     int fd;
-
     fd = open(dev, O_RDONLY);
+
     if (fd == -1) {
         printw("Cannot open keyboard device %s: %s.\n", dev, strerror(errno));
     }
@@ -277,6 +301,20 @@ void agentStep(){
    		alei.act(PLAYER_A_DOWNRIGHT);
    		break;
    	
+   	case 11100:
+   		alei.act(PLAYER_A_LEFT);
+   		break;
+   	case 11010:
+   		alei.act(PLAYER_A_RIGHT);
+   		break;
+   	case 10110:
+   		alei.act(PLAYER_A_UP);
+   		break;
+   	case 1110:
+   		alei.act(PLAYER_A_DOWN);
+   		break;
+
+
    	case 1:
    		alei.act(PLAYER_A_FIRE);
    		break;
@@ -306,7 +344,20 @@ void agentStep(){
    		alei.act(PLAYER_A_DOWNRIGHTFIRE);
    		break;
 
+	case 11101:
+   		alei.act(PLAYER_A_LEFTFIRE);
+   		break;
+   	case 11011:
+   		alei.act(PLAYER_A_RIGHTFIRE);
+   		break;
+   	case 10111:
+   		alei.act(PLAYER_A_UPFIRE);
+   		break;
+   	case 1111:
+   		alei.act(PLAYER_A_DOWNFIRE);
+   		break;
    	default:
+	   	alei.act(PLAYER_A_NOOP);
    		break;
    }
    
@@ -364,16 +415,14 @@ void agentStep(){
    else if(keys[KEY_FIRE]) {
     alei.act(PLAYER_A_FIRE);
    }
-	*/
-   //Last, a requirement of the library, to prevent strange behaviors
-   alei.act(PLAYER_A_NOOP);
+	*/   
 }
 
 void initRamVector() {
-   for(unsigned i=0; i<128; i++) {
-      previousRam[i] = -1;
-      previousRAMFrames[i] = -1;
-   }
+	for(unsigned i=0; i<128; i++) {
+	  previousRam[i] = -1;
+	  previousRAMFrames[i] = -1;
+	}
 }
 
 string parseName(char* nameOfGame) {
@@ -394,67 +443,62 @@ string parseName(char* nameOfGame) {
 }
 
 void initialize(char* nameOfGame) {
-   struct sigaction sigIntHandler;
+	struct sigaction sigIntHandler;
 
-   sigIntHandler.sa_handler = ctrlCHandler;
-   sigemptyset(&sigIntHandler.sa_mask);
-   sigIntHandler.sa_flags = 0;
+	sigIntHandler.sa_handler = ctrlCHandler;
+	sigemptyset(&sigIntHandler.sa_mask);
+	sigIntHandler.sa_flags = 0;
 
-   sigaction(SIGINT, &sigIntHandler, NULL);
+	sigaction(SIGINT, &sigIntHandler, NULL);
 
-   for(int i=0; i<N_KEYS; ++i) {
-      keys[i] = false;
-   }
+	for(int i=0; i<N_KEYS; ++i) {
+	  keys[i] = false;
+	}
 
-   delayToFPS = 1000 / FPS;
+	delayToFPS = 1000 / FPS;
 
-   initRamVector();
+	initRamVector(); 
+	std::string name= parseName(nameOfGame);
 
-   std::string name = parseName(nameOfGame);
-
-   file.open(RAM_FILE+name+FILE_TERMINATION);
-   fileBin.open(RAM_FILE_BINARY+name+FILE_TERMINATION);
+	remove( (RAM_FILE+name+FILE_TERMINATION).c_str() );//it erases the file with previous file with the same name of the game
+	remove( (RAM_FILE_BINARY+name+FILE_TERMINATION).c_str() );
 }
 
-
-
 int main(int argc, char **argv) {
-   ///////
+	// Check input parameter
+	if (argc != 2) {
+	  usage(argv[0]);
+	  return -1;
+	}
 
-   // Check input parameter
-   if (argc != 2) {
-      usage(argv[0]);
-      return -1;
-   }
+	initialize(argv[1]);
 
-   initialize(argv[1]);
+	// Create alei object.
+	alei.setInt("random_seed", SEED);
+	alei.setFloat("repeat_action_probability", 0);
+	alei.setBool("display_screen", SCREEN);
+	alei.setBool("sound", SOUND);
+	alei.loadROM(argv[1]);   
 
-   // Create alei object.
-   alei.setInt("random_seed", SEED);
-   alei.setFloat("repeat_action_probability", 0);
-   alei.setBool("display_screen", SCREEN);
-   alei.setBool("sound", SOUND);
-   alei.loadROM(argv[1]);   
+	STDP::Inicializar();
 
-   //STDP::Inicializar();
+	//Starting to read keyboard
+	lector = std::thread(keyboardRead);
+	std::string name= parseName(argv[1]);
+	int step;
+	while (!alei.game_over()) 
+	{
 
-   //Starting to read keyboard
-   lector = std::thread(keyboardRead);
-   
-   int step;
-   for (step = 0; 
-        !alei.game_over() && step < MAX_STEPS; 
-        ++step) 
-   {
-     // printRam();
-      agentStep();
-      ramToFile();
+		printRam();
+		agentStep();
+		ramToFile(name);
 
-      std::this_thread::sleep_for (std::chrono::milliseconds(delayToFPS));
-   }
-   
-   lector.detach();
-   STDP::Terminar();
+		std::this_thread::sleep_for (std::chrono::milliseconds(delayToFPS));
+	}
 
-   std::cout << "-- PROGRAM FINISHED --\n";
+	lector.detach();
+	STDP::Terminar();
+
+	std::cout << "-- PROGRAM FINISHED --\n";
+	return 0;
 }
